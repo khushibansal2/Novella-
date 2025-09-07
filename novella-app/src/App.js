e63e9ec5bcbe78504db814db6b2d332288b19a61
Khushi Bansal <khushibansal4207@gmail.com>
2025-09-07 15:43:46 +0530

    Add initial Novella app files

---
+++ novella-app/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 Welcome to Novella</h1>
+      <p>Your app is running locally now!</p>
+    </div>
+  );
+}
+
+export default App;
